233a2d3ac39eb3f648d6a21d38126f14ca65c9da
Nagavamshi10 <vamshi.battu@applaudhr.com>
2025-08-20 21:33:58 +0530

    optimse the book.js file

--- common/models/book.js
+++ common/models/book.js
@@ -1,117 +1,112 @@
 'use strict';
 
 module.exports = function(Book) {
-    // to check for multiple records
-    Book.beforeRemote('create', function(ctx, data, next) {
-        //console.log(ctx.req.body);
-      const myUniquePropertyValue = ctx.req.body.title;
-  
-      Book.findOne({ where: { title: myUniquePropertyValue } }, function(err, result) {
-        if (err) return next(err);
-        if (result) {
-          const error = new Error(` '${myUniquePropertyValue}' already exists in the DB`);
-          error.statusCode = 422;
-          return next(error);
-        }
-        next();
-      });
+  // Check for duplicate records before creating a book
+  Book.beforeRemote('create', function(ctx, data, next) {
+    const bookTitle = ctx.req.body.title;
+    
+    if (!bookTitle) {
+      const error = new Error('Book title is required');
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    Book.findOne({ where: { title: bookTitle } }, function(err, existingBook) {
+      if (err) return next(err);
+      
+      if (existingBook) {
+        const error = new Error(`Book '${bookTitle}' already exists in the database`);
+        error.statusCode = 422;
+        return next(error);
+      }
+      
+      next();
     });
+  });
 
+  // Convert user IDs to usernames in the response
+  Book.afterRemote('find', function(ctx, books, next) {
+    if (!books || books.length === 0) {
+      return next();
+    }
 
-    // change userid to usernames
-    Book.afterRemote('find', function(ctx, data, next) {
-      //console.log("ctx data");
-      //console.log(ctx);
-      Book.app.models.Users.find((err,result)=>{
-        if(err){
-          console.log(err);
-        }
-        const array=result;
-       // console.log(result);
-        for(let i=0;i<data.length;i++){
-          //console.log("1");
-              for(let j=0;j<data[i].AssignedTo.length;j++){
-                //console.log("2");
-                // console.log(data[i].AssignedTo[j])
-                array.find(o => {
-                  //console.log(o);
-                  if(o.id.toString() == data[i].AssignedTo[j]) {
-                    //console.log(o.Username);
-                    data[i].AssignedTo[j]=o.Username;
-                  }
-                });
-                
-              }
-          }
-          //console.log('final data',data[0].AssignedTo);
-          next();
-        //});
-        //console.log('final data',data);
+    Book.app.models.Users.find(function(err, users) {
+      if (err) {
+        return next(err);
+      }
+
+      // Create a lookup map for O(1) user ID to username conversion
+      const userLookupMap = new Map();
+      users.forEach(user => {
+        userLookupMap.set(user.id.toString(), user.Username);
       });
-    });
 
-    // to delete the book
-    Book.deleteBook=function(title,cb)
-       {
-        console.log('title',title);
-        Book.findOne({where:{title: title}
-        }, function(err, user) {
-            if (err) throw err;
-            console.log(user);
-            //console.log(user.__data.id);
-            if(user==null){
-                return cb(null,"No such Book");
-            }
-            else{
-            Book.destroyById(user.__data.id,cb);
-            }
+      // Convert user IDs to usernames
+      books.forEach(book => {
+        if (book.AssignedTo && Array.isArray(book.AssignedTo)) {
+          book.AssignedTo = book.AssignedTo.map(userId => {
+            return userLookupMap.get(userId.toString()) || userId;
           });
-       }
-    
-       Book.remoteMethod('deleteBook',
-      {
-        accepts:{arg:'title',type:'string',required:true},
-        returns:{arg:'Message',type:'string'},
-        http:{path:'/deleteBook',verb:'post'}
+        }
       });
 
+      next();
+    });
+  });
 
+  // Delete a book by title
+  Book.deleteBook = function(title, callback) {
+    if (!title) {
+      return callback(new Error('Book title is required'));
+    }
 
-      Book.CountBook=function(cb,err)
-       {
-        
-        Book.find({},function(err,res){
-          if(err){
-            console.log(err);
-            cb(err);
-          }
-          let avaialblecount=0;
-          let assigncount=0;
-          let individualbooks=res.length;
-          for(let i=0;i<res.length;i++){
-            avaialblecount+=res[i].Nocount;
-            assigncount+=res[i].AssignedTo.length;
-          }
-          
-          // cb(test);
-         return cb(err,{avaialblecount,assigncount,individualbooks});
-          //console.log(individualbooks);
-        })
-       }
-    
-       Book.remoteMethod('CountBook',
-      {
-        returns:{arg:'Message',type:'object'},
-        http:{path:'/CountBook',verb:'get'}
+    Book.findOne({ where: { title: title } }, function(err, book) {
+      if (err) {
+        return callback(err);
+      }
+
+      if (!book) {
+        return callback(null, 'No such book found');
+      }
+
+      Book.destroyById(book.id, function(err) {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, `Book '${title}' deleted successfully`);
+      });
+    });
+  };
+
+  Book.remoteMethod('deleteBook', {
+    accepts: { arg: 'title', type: 'string', required: true },
+    returns: { arg: 'Message', type: 'string' },
+    http: { path: '/deleteBook', verb: 'post' }
+  });
+
+  // Get book count statistics
+  Book.CountBook = function(callback) {
+    Book.find({}, function(err, books) {
+      if (err) {
+        return callback(err);
+      }
+
+      const stats = books.reduce((acc, book) => {
+        acc.availableCount += book.Nocount || 0;
+        acc.assignedCount += (book.AssignedTo && book.AssignedTo.length) || 0;
+        return acc;
+      }, {
+        availableCount: 0,
+        assignedCount: 0,
+        totalBooks: books.length
       });
 
-      // update purpose
-//       User.observe('after save', function(ctx, next) {
-//         // Check if this is a create operation
-//         if (ctx.isNewInstance) {
-//           const user = ctx.instance;
-//         }
-//         // Call the next function to continue
-//         next();
-//       });
+      callback(null, stats);
+    });
+  };
+
+  Book.remoteMethod('CountBook', {
+    returns: { arg: 'Message', type: 'object' },
+    http: { path: '/CountBook', verb: 'get' }
+  });
 };
